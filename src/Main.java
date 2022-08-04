d6c7dcc7f7bee028922f803551e8cb1e8c663746
Алекс <dendyboy777@gmail.com>
2022-08-04 20:37:00 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        // ЗАдание 1
+        byte z = 1;
+        short x = 1000;
+        int c = 100000;
+        long v = 100000000l;
+        double b = 3.14;
+        float n = 3.1415f;
+        boolean m = x > 2000;
+        char a =(36);
+System.out.println(a);}}
+// задфние 2
