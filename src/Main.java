0bf02b4122f488ab6c7c73339f4a9fba47072a1e
Алекс <dendyboy777@gmail.com>
2022-08-05 20:15:44 +0300

    задание 1-3

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,24 @@ public class Main {
         float n = 3.1415f;
         boolean m = x > 2000;
         char a =(36);
-System.out.println(a);}}
-// задфние 2
+
+        // Задание 2
+double boxer1 = 78.2;
+double boxer2 = 82.7;
+double weightOfAll = boxer1 + boxer2;
+double weightDifference = boxer2 - boxer1;
+System.out.println("Разница в весе " + weightDifference);
+System.out.println("Общий вес " + weightOfAll);
+
+        // Задание 3
+    byte numberOfBananas = 5;
+    short weightOfOneBanana = 80;
+    short milkWeightInMilliliters = 200;
+    short oneMilliliterOfMilkInGrams = 105/100;
+    byte numberOfIceCream = 2;
+    byte weightOfIceCream = 100;
+    byte numberOfEggs = 4;
+    byte weightOfEggs = 70;
+    float weightOfAllIngredientsInGrams = numberOfBananas * weightOfOneBanana + milkWeightInMilliliters *oneMilliliterOfMilkInGrams + numberOfIceCream * weightOfIceCream +numberOfEggs * weightOfEggs;
+   float weightOfAllIngredientsInKilograms = weightOfAllIngredientsInGrams / 1000;
+   System.out.println("Общий вес ингридиентов в кг " +weightOfAllIngredientsInKilograms);}}
